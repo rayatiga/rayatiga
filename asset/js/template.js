a5874f5913d19d31428db32d1e35efa3787c8b47
bydzen <svnteentthones@gmail.com>
2023-04-15 21:56:31 +0700

    update aria-current nav

--- asset/js/template.js
+++ asset/js/template.js
@@ -4,6 +4,7 @@ var nav = document.getElementsByTagName('nav')[0]
 if (nav) {
     var state
     var active = ' active'
+    var aria = ' aria-current="page"'
     var path = window.location.pathname
     nav.outerHTML = `<nav class="navbar navbar-expand-lg bg-light border-bottom fixed-top">
             <div class="container py-2">
@@ -13,17 +14,17 @@ if (nav) {
                 </button>
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul class="navbar-nav">
-                        <li class="nav-item"><a class="nav-link${path == '/' ? (state = active) : (state = '')}" href="/">Home</a></li>
-                        <li class="nav-item"><a class="nav-link${path == '/about/' ? (state = active) : (state = '')}" href="/about/">About</a></li>
-                        <li class="nav-item"><a class="nav-link${path == '/career/' ? (state = active) : (state = '')}" href="/career/">Career</a></li>
+                        <li class="nav-item"><a class="nav-link${path == '/' ? (state = active) : (state = '')}"${path == '/' ? (state = aria) : (state = '')} href="/">Home</a></li>
+                        <li class="nav-item"><a class="nav-link${path == '/about/' ? (state = active) : (state = '')}"${path == '/about/' ? (state = aria) : (state = '')} href="/about/">About</a></li>
+                        <li class="nav-item"><a class="nav-link${path == '/career/' ? (state = active) : (state = '')}"${path == '/career/' ? (state = aria) : (state = '')} href="/career/">Career</a></li>
                         <li class="nav-item dropdown">
                             <a class="nav-link${path == '/hosting/' || path == '/hosting/about/' || path == '/hosting/contact/' || path == '/hosting/pricing/' ? (state = active) : (state = '')} dropdown-toggle" href="#" role="button" data-root-toggle="dropdown" aria-expanded="false">Service&nbsp;</a>
                             <ul class="dropdown-menu bg-light">
                                 <li><h6 class="dropdown-header">Web Hosting</h6></li>
-                                <li><a class="dropdown-item${path == '/hosting/' ? (state = active) : (state = '')}" href="/hosting/">Home</a></li>
-                                <li><a class="dropdown-item${path == '/hosting/about/' ? (state = active) : (state = '')}" href="/hosting/about/">About</a></li>
-                                <li><a class="dropdown-item${path == '/hosting/contact/' ? (state = active) : (state = '')}" href="/hosting/contact/">Contact</a></li>
-                                <li><a class="dropdown-item${path == '/hosting/pricing/' ? (state = active) : (state = '')}" href="/hosting/pricing/">Pricing</a></li>
+                                <li><a class="dropdown-item${path == '/hosting/' ? (state = active) : (state = '')}"${path == '/hosting/' ? (state = aria) : (state = '')} href="/hosting/">Home</a></li>
+                                <li><a class="dropdown-item${path == '/hosting/about/' ? (state = active) : (state = '')}"${path == '/hosting/about/' ? (state = aria) : (state = '')} href="/hosting/about/">About</a></li>
+                                <li><a class="dropdown-item${path == '/hosting/contact/' ? (state = active) : (state = '')}"${path == '/hosting/contact/' ? (state = aria) : (state = '')} href="/hosting/contact/">Contact</a></li>
+                                <li><a class="dropdown-item${path == '/hosting/pricing/' ? (state = active) : (state = '')}"${path == '/hosting/pricing/' ? (state = aria) : (state = '')} href="/hosting/pricing/">Pricing</a></li>
                                 <li class="dropdown-divider"></li>
                                 <li><h6 class="dropdown-header">Promotion</h6></li>
                                 <li><a class="dropdown-item" href="/hosting/pricing/#domain">Domain Name</a></li>
@@ -35,15 +36,15 @@ if (nav) {
                             <a class="nav-link${path == '/project/' || path == '/project/apache2-benchmark.html' || path == '/project/php-ini-configuration.html' || path == '/project/wordpress-installer.html' || path == '/project/apache2-benchmark/' || path == '/project/php-ini-configuration/' || path == '/project/wordpress-installer/' ? (state = active) : (state = '')} dropdown-toggle" href="#" role="button" data-root-toggle="dropdown" aria-expanded="false">Project&nbsp;</a>
                             <ul class="dropdown-menu bg-light">
                                 <li><h6 class="dropdown-header">Top Three</h6></li>
-                                <li><a class="dropdown-item${path == '/project/apache2-benchmark.html' ? (state = active) : (state = '')}" href="/project/apache2-benchmark.html">Apache2 Benchmark</a></li>
-                                <li><a class="dropdown-item${path == '/project/php-ini-configuration.html' ? (state = active) : (state = '')}" href="/project/php-ini-configuration.html">PHP ini Configuration</a></li>
-                                <li><a class="dropdown-item${path == '/project/wordpress-installer.html' ? (state = active) : (state = '')}" href="/project/wordpress-installer.html">WordPress Installer</a></li>
+                                <li><a class="dropdown-item${path == '/project/apache2-benchmark.html' ? (state = active) : (state = '')}"${path == '/project/apache2-benchmark.html' ? (state = aria) : (state = '')} href="/project/apache2-benchmark.html">Apache2 Benchmark</a></li>
+                                <li><a class="dropdown-item${path == '/project/php-ini-configuration.html' ? (state = active) : (state = '')}"${path == '/project/php-ini-configuration.html' ? (state = aria) : (state = '')} href="/project/php-ini-configuration.html">PHP ini Configuration</a></li>
+                                <li><a class="dropdown-item${path == '/project/wordpress-installer.html' ? (state = active) : (state = '')}"${path == '/project/wordpress-installer.html' ? (state = aria) : (state = '')} href="/project/wordpress-installer.html">WordPress Installer</a></li>
                                 <li class="dropdown-divider"></li>
-                                <li><a class="dropdown-item${path == '/project/' ? (state = active) : (state = '')}" href="/project/">Other</a></li>
+                                <li><a class="dropdown-item${path == '/project/' ? (state = active) : (state = '')}"${path == '/' ? (state = aria) : (state = '')} href="/project/">Other</a></li>
                             </ul>
                         </li>
-                        <li class="nav-item d-lg-none"><a class="nav-link${path == '/contact/' ? (state = active) : (state = '')}" href="/contact/">Contact</a></li>
-                        <li class="nav-item d-none d-lg-block"><a class="btn btn-dark ms-2${path == '/contact/' ? (state = active) : (state = '')}" href="/contact/">Contact</a></li>
+                        <li class="nav-item d-lg-none"><a class="nav-link${path == '/contact/' ? (state = active) : (state = '')}"${path == '/contact/' ? (state = aria) : (state = '')} href="/contact/">Contact</a></li>
+                        <li class="nav-item d-none d-lg-block"><a class="btn btn-dark ms-2${path == '/contact/' ? (state = active) : (state = '')}"${path == '/contact/' ? (state = aria) : (state = '')} href="/contact/">Contact</a></li>
                     </ul>
                 </div>
             </div>
