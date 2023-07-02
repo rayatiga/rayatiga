b18f0d6f943d048721a1fc0aa1b83691d23ed73c
bydzen <svnteentthones@gmail.com>
2023-07-03 00:03:11 +0700

    simplicity and major changes

--- public/asset/js/template.js
+++ public/asset/js/template.js
@@ -1,5 +1,3 @@
-// JavaScript Template
-// Navigation Template
 let nav = document.getElementsByTagName("nav")[0]
 if (nav) {
     let state
@@ -14,20 +12,38 @@ if (nav) {
                 </button>
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul class="navbar-nav">
-                        <li class="nav-item"><a class="nav-link${path == "/" ? (state = active) : (state = "")}"${path == "/" ? (state = aria) : (state = "")} href="/">Home</a></li>
-                        <li class="nav-item"><a class="nav-link${path == "/about/" ? (state = active) : (state = "")}"${path == "/about/" ? (state = aria) : (state = "")} href="/about/">About</a></li>
-                        <li class="nav-item"><a class="nav-link${path == "/blog/" ? (state = active) : (state = "")}"${path == "/blog/" ? (state = aria) : (state = "")} href="/blog/">Blog</a></li>
-                        <li class="nav-item"><a class="nav-link${path == "/career/" ? (state = active) : (state = "")}"${path == "/career/" ? (state = aria) : (state = "")} href="/career/">Career</a></li>
+                        <li class="nav-item"><a class="nav-link${path == "/" ? (state = active) : (state = "")}"${
+        path == "/" ? (state = aria) : (state = "")
+    } href="/">Home</a></li>
+                        <li class="nav-item"><a class="nav-link${path == "/about/" ? (state = active) : (state = "")}"${
+        path == "/about/" ? (state = aria) : (state = "")
+    } href="/about/">About</a></li>
+                        <li class="nav-item"><a class="nav-link${path == "/blog/" ? (state = active) : (state = "")}"${
+        path == "/blog/" ? (state = aria) : (state = "")
+    } href="/blog/">Blog</a></li>
+                        <li class="nav-item"><a class="nav-link${path == "/career/" ? (state = active) : (state = "")}"${
+        path == "/career/" ? (state = aria) : (state = "")
+    } href="/career/">Career</a></li>
                         <li class="nav-item dropdown">
                             <a class="nav-link${
-                                path == "/hosting/" || path == "/hosting/about/" || path == "/hosting/contact/" || path == "/hosting/pricing/" ? (state = active) : (state = "")
+                                path == "/hosting/" || path == "/hosting/about/" || path == "/hosting/contact/" || path == "/hosting/pricing/"
+                                    ? (state = active)
+                                    : (state = "")
                             } dropdown-toggle" href="#" role="button" data-root-toggle="dropdown" aria-expanded="false">Service&nbsp;</a>
                             <ul class="dropdown-menu bg-light">
                                 <li><h6 class="dropdown-header">Web Hosting</h6></li>
-                                <li><a class="dropdown-item${path == "/hosting/" ? (state = active) : (state = "")}"${path == "/hosting/" ? (state = aria) : (state = "")} href="/hosting/">Home</a></li>
-                                <li><a class="dropdown-item${path == "/hosting/about/" ? (state = active) : (state = "")}"${path == "/hosting/about/" ? (state = aria) : (state = "")} href="/hosting/about/">About</a></li>
-                                <li><a class="dropdown-item${path == "/hosting/contact/" ? (state = active) : (state = "")}"${path == "/hosting/contact/" ? (state = aria) : (state = "")} href="/hosting/contact/">Contact</a></li>
-                                <li><a class="dropdown-item${path == "/hosting/pricing/" ? (state = active) : (state = "")}"${path == "/hosting/pricing/" ? (state = aria) : (state = "")} href="/hosting/pricing/">Pricing</a></li>
+                                <li><a class="dropdown-item${path == "/hosting/" ? (state = active) : (state = "")}"${
+        path == "/hosting/" ? (state = aria) : (state = "")
+    } href="/hosting/">Home</a></li>
+                                <li><a class="dropdown-item${path == "/hosting/about/" ? (state = active) : (state = "")}"${
+        path == "/hosting/about/" ? (state = aria) : (state = "")
+    } href="/hosting/about/">About</a></li>
+                                <li><a class="dropdown-item${path == "/hosting/contact/" ? (state = active) : (state = "")}"${
+        path == "/hosting/contact/" ? (state = aria) : (state = "")
+    } href="/hosting/contact/">Contact</a></li>
+                                <li><a class="dropdown-item${path == "/hosting/pricing/" ? (state = active) : (state = "")}"${
+        path == "/hosting/pricing/" ? (state = aria) : (state = "")
+    } href="/hosting/pricing/">Pricing</a></li>
                                 <li class="dropdown-divider"></li>
                                 <li><h6 class="dropdown-header">Promotion</h6></li>
                                 <li><a class="dropdown-item" href="/hosting/pricing/#domain">Domain Name</a></li>
@@ -59,17 +75,22 @@ if (nav) {
         path == "/project/wordpress-installer.html" ? (state = aria) : (state = "")
     } href="/project/wordpress-installer.html">WordPress Installer</a></li>
                                 <li class="dropdown-divider"></li>
-                                <li><a class="dropdown-item${path == "/project/" ? (state = active) : (state = "")}"${path == "/" ? (state = aria) : (state = "")} href="/project/">Other</a></li>
+                                <li><a class="dropdown-item${path == "/project/" ? (state = active) : (state = "")}"${
+        path == "/" ? (state = aria) : (state = "")
+    } href="/project/">Other</a></li>
                             </ul>
                         </li>
-                        <li class="nav-item d-lg-none"><a class="nav-link${path == "/contact/" ? (state = active) : (state = "")}"${path == "/contact/" ? (state = aria) : (state = "")} href="/contact/">Contact</a></li>
-                        <li class="nav-item d-none d-lg-block"><a class="btn btn-dark ms-2${path == "/contact/" ? (state = active) : (state = "")}"${path == "/contact/" ? (state = aria) : (state = "")} href="/contact/">Contact</a></li>
+                        <li class="nav-item d-lg-none"><a class="nav-link${path == "/contact/" ? (state = active) : (state = "")}"${
+        path == "/contact/" ? (state = aria) : (state = "")
+    } href="/contact/">Contact</a></li>
+                        <li class="nav-item d-none d-lg-block"><a class="btn btn-dark ms-2${path == "/contact/" ? (state = active) : (state = "")}"${
+        path == "/contact/" ? (state = aria) : (state = "")
+    } href="/contact/">Contact</a></li>
                     </ul>
                 </div>
             </div>
         </nav>`
 }
-// Footer Template
 let footer = document.getElementsByTagName("footer")[0]
 if (footer) {
     footer.outerHTML = `<footer>
@@ -78,19 +99,14 @@ if (footer) {
             </div>
         </footer>`
 }
-// Checkout Page
-// Checkout Template
 let checkout = document.getElementById("checkoutPage")
 if (checkout) {
-    checkout.innerHTML = `<!-- Navigation Element -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom fixed-top">
+    checkout.innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom fixed-top">
             <div class="container justify-content-center">
                 <a class="navbar-brand fw-bold m-0" href="/">Rayatiga</a>
             </div>
         </nav>
-        <!-- Header Element -->
         <header><div></div></header>
-        <!-- Main Element -->
         <main>
             <div class="container min-vh-100 bg-light d-flex flex-column justify-content-center align-items-center text-center">
                 <h1 class="fw-bold">Checkouting...</h1>
@@ -99,7 +115,6 @@ if (checkout) {
             </div>
         </main>`
 }
-// Checkout Countdown Timer
 let checkoutTimerSpan = document.getElementById("checkoutTimer")
 if (checkoutTimerSpan) {
     checkoutTimer()
@@ -114,7 +129,6 @@ function checkoutTimer() {
         }
     }, 1000)
 }
-// Button Cancel Checkout
 function closeWindow() {
     window.open("", "_self", "")
     window.close()
