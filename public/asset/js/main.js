d7b1fe0e8321d85bd1324d36c2b1f27b8c99c917
bydzen <svnteentthones@gmail.com>
2023-07-26 16:14:54 +0700

    major changes #20230726

--- public/asset/js/main.js
+++ public/asset/js/main.js
@@ -1,40 +1,39 @@
-var scrollTopButton = document.getElementById('scrollTop')
+// Scroll to Top
+var scrollTopButton = document.getElementById('scrollTopButton')
 if (scrollTopButton) {
-    window.onscroll = function () {
-        showScrollButton()
+  window.onscroll = function () {
+    scrollTopButtonShow()
+  }
+  function scrollTopButtonShow() {
+    if (scrollTopButton) {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollTopButton.style.opacity = '1'
+      } else {
+        scrollTopButton.style.opacity = '0'
+      }
     }
-    function showScrollButton() {
-        if (scrollTopButton) {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                scrollTopButton.style.opacity = '1'
-            } else {
-                scrollTopButton.style.opacity = '0'
-            }
-        }
-    }
-    function scrollTopAction() {
-        document.body.scrollTop = 0
-        document.documentElement.scrollTop = 0
-    }
+  }
+  function scrollTopButtonAction() {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+  }
 }
+// Checkout Timer
 let checkoutTimerSpan = document.getElementById('checkoutTimer')
 if (checkoutTimerSpan) {
-    checkoutTimer()
+  checkoutTimer()
 }
 function checkoutTimer() {
-    let countdown = 5
-    let interval = setInterval(function () {
-        checkoutTimerSpan.innerHTML = --countdown
+  let countdown = 5
+  let interval = setInterval(function () {
+    checkoutTimerSpan.innerHTML = --countdown
 
-        if (countdown <= 0) {
-            clearInterval(interval)
-        }
-    }, 1000)
+    if (countdown <= 0) {
+      clearInterval(interval)
+    }
+  }, 1000)
 }
 function closeWindow() {
-    window.open('', '_self', '')
-    window.close()
+  window.open('', '_self', '')
+  window.close()
 }
-
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl))
