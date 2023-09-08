ade47baa80b57235dd90915ef1d7d224e9cfd42c
sultan <svnteentthones@gmail.com>
2023-09-08 08:17:06 +0700

    Wrangler interactive mode

--- wrangler-deploy.sh
+++ wrangler-deploy.sh
@@ -1,3 +1,5 @@
 echo "Deploying to: rayatiga"
 read -p "Enter commit message: " commitMessage
-wrangler pages deploy public/ --project-name "rayatiga" --commit-message "$commitMessage"
+read -p "Enter directory to deploy: public/" directoryDeploy
+echo "Deploy from directory public/$directoryDeploy with commit message $commitMessage"
+wrangler pages deploy public/$directoryDeploy --project-name "rayatiga" --commit-message "$commitMessage"
