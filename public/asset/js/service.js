ced545211225f12c6011627f2de82ff0c23c1ba9
bydzen <svnteentthones@gmail.com>
2023-10-28 07:20:27 +0700

    create service to whatsapp

---
+++ public/asset/js/service.js
@@ -0,0 +1,42 @@
+function selectService(service) {
+  switch (service) {
+    case 'domain-com':
+      window.open('https://wa.me/6282122220362?text=Hello+Rayatiga.+I+want+domain+.COM+package.')
+      break
+    case 'domain-id':
+      window.open('https://wa.me/6282122220362?text=Hello+Rayatiga.+I+want+domain+.ID+package.')
+      break
+    case 'domain-coid':
+      window.open('https://wa.me/6282122220362?text=Hello+Rayatiga.+I+want+domain+.CO.ID+package.')
+      break
+    case 'static-a':
+      window.open('https://wa.me/6282122220362?text=Hello+Rayatiga.+I+want+Static+A+package.')
+      break
+    case 'static-b':
+      window.open('https://wa.me/6282122220362?text=Hello+Rayatiga.+I+want+Static+B+package.')
+      break
+    case 'static-c':
+      window.open('https://wa.me/6282122220362?text=Hello+Rayatiga.+I+want+Static+C+package.')
+      break
+    case 'wordpress-a':
+      window.open('https://wa.me/6282122220362?text=Hello+Rayatiga.+I+want+WordPress+A+package.')
+      break
+    case 'wordpress-b':
+      window.open('https://wa.me/6282122220362?text=Hello+Rayatiga.+I+want+WordPress+B+package.')
+      break
+    case 'wordpress-c':
+      window.open('https://wa.me/6282122220362?text=Hello+Rayatiga.+I+want+WordPress+C+package.')
+      break
+    case 'cloud-a':
+      window.open('https://wa.me/6282122220362?text=Hello+Rayatiga.+I+want+Cloud+A+package.')
+      break
+    case 'cloud-b':
+      window.open('https://wa.me/6282122220362?text=Hello+Rayatiga.+I+want+Cloud+B+package.')
+      break
+    case 'cloud-c':
+      window.open('https://wa.me/6282122220362?text=Hello+Rayatiga.+I+want+Cloud+C+package.')
+      break
+    default:
+      break
+  }
+}
