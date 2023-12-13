612beccd3197f32e64f496487ec9db0e2e857544
bydzen <svnteentthones@gmail.com>
2023-12-13 09:31:05 +0700

    change form validate to global script

--- public/asset/js/main.js
+++ public/asset/js/main.js
@@ -12,3 +12,20 @@ popoverTriggerList.forEach((popoverTriggerEl) => {
 });
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
+(() => {
+  "use strict";
+  const forms = document.querySelectorAll(".needs-validation");
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
