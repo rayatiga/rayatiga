f21aa1586c57a8ad179f8a1f44ec04956900893d
bydzen <svnteentthones@gmail.com>
2024-01-06 18:01:23 +0700

    remove navigation event worker

--- public/asset/js/main.js
+++ public/asset/js/main.js
@@ -26,36 +26,3 @@ const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootst
     );
   });
 })();
-// Event navbar clicked
-const navToggle = document.querySelector("button.navbar-toggler");
-const navMain = document.querySelector("nav.navbar");
-navMain.style.transition = "background 0.25s ease-in-out";
-let navToggleTrigger = false;
-const navMainStyled = () => {
-  navMain.classList.add("bg-body-tertiary");
-  navMain.classList.add("border-bottom");
-};
-const navMainTransparent = () => {
-  navMain.classList.remove("bg-body-tertiary");
-  navMain.classList.remove("border-bottom");
-};
-navToggle.onclick = function () {
-  navToggle.disabled = true;
-  setTimeout(() => {
-    navToggle.disabled = false;
-  }, 350);
-  if (!navToggleTrigger && window.scrollY < 16) {
-    navMainStyled();
-    navToggleTrigger = true;
-  } else if (window.scrollY < 16) {
-    navMainTransparent();
-    navToggleTrigger = false;
-  }
-};
-window.addEventListener("scroll", function () {
-  if (this.window.scrollY >= 16) {
-    navMainStyled();
-  } else {
-    navMainTransparent();
-  }
-});
