7c73512eebc45e85ccaffd9d2281345d42cd02ab
bydzen <svnteentthones@gmail.com>
2024-02-29 11:30:13 +0700

    Add code information

--- public/asset/js/post.js
+++ public/asset/js/post.js
@@ -1,5 +1,5 @@
-/* Post JS */
-// Default: Function Measuring Reading Time
+/* File: post.js */
+/* Default: Function Measuring Reading Time */
 function readingTime() {
   const text = document.querySelector("article").innerText;
   const wpm = 265;
@@ -8,7 +8,7 @@ function readingTime() {
   document.getElementById("time").innerText = time;
 }
 readingTime();
-// Default: Event Copy Article
+/* Default: Copy Article */
 const copyArticleBtn = document.getElementById("copy");
 const copyArticleBtnText = document.getElementById("copy-text");
 const articleTitle = document.querySelector("article h1").textContent;
@@ -22,7 +22,7 @@ copyArticleBtn.addEventListener("click", () => {
     copyArticleBtnText.textContent = originalText;
   }, 2000);
 });
-// Default: Event Share Article
+/* Default: Share Article */
 const shareArticleBtn = document.getElementById("share");
 shareArticleBtn.addEventListener("click", () => {
   if (navigator.share) {
