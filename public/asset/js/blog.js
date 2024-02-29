e09d2f94e986d6a7b627456777eddabbc24189b7
bydzen <svnteentthones@gmail.com>
2024-02-29 11:30:47 +0700

    Add code information

--- public/asset/js/blog.js
+++ public/asset/js/blog.js
@@ -1,5 +1,5 @@
-/* Blog JS */
-// Default: Function Trim Text
+/* File: blog.js */
+/* Default: Function Trim Text */
 function trimText(text, maxLength) {
   if (text.length <= maxLength) {
     return text;
@@ -7,13 +7,13 @@ function trimText(text, maxLength) {
     return text.substring(0, maxLength) + "...";
   }
 }
-// Default: Trim Card Post Title
+/* Default: Trim Card Title */
 const cardPostTitle = document.querySelectorAll("#blog .card a");
 for (let i = 0; i < cardPostTitle.length; i++) {
   const trimmedTitleText = trimText(cardPostTitle[i].textContent, 32);
   cardPostTitle[i].textContent = trimmedTitleText;
 }
-// Default: Trim Card Post Description
+/* Default: Trim Card Description */
 const cardPostDescription = document.querySelectorAll("#blog .card p");
 for (let i = 0; i < cardPostDescription.length; i++) {
   const trimmedDescriptionText = trimText(cardPostDescription[i].textContent, 128);
