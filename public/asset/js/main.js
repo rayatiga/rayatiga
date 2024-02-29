46624d40fd619a85a6f1e3c2b1a62442ff2438d4
bydzen <svnteentthones@gmail.com>
2024-02-29 11:30:17 +0700

    Add code information

--- public/asset/js/main.js
+++ public/asset/js/main.js
@@ -1,14 +1,28 @@
-/* Main JS */
-// Bootstrap: Popover
+/* File: main.js */
+/* Default: Scroll to Top */
+document.body.insertAdjacentHTML("beforeend", '<button class="btn bg-body border position-fixed shadow-sm" id="scroll-to-top" style="bottom: 24px; right: 24px;" title="Scroll to Top"><i class="bi bi-arrow-up"></i></button>');
+const scrollTopBtn = document.getElementById("scroll-to-top");
+scrollTopBtn.style.display = "none";
+scrollTopBtn.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 128) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+});
+/* Framework: Bootstrap: Popover */
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
 const popoverList = [...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl));
 popoverTriggerList.forEach((popoverTriggerEl) => {
   popoverTriggerEl.style.cursor = "pointer";
 });
-// Bootstrap: Tooltip
+/* Framework: Bootstrap: Tooltip */
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
-// Bootstrap: Form Validation
+/* Framework: Bootstrap: Form Validation */
 (() => {
   "use strict";
   const forms = document.querySelectorAll(".needs-validation");
@@ -26,17 +40,3 @@ const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootst
     );
   });
 })();
-// Default: Scroll to Top
-document.body.insertAdjacentHTML("beforeend", '<button class="btn bg-body border position-fixed shadow-sm" id="scroll-to-top" style="bottom: 24px; right: 24px;" title="Scroll to Top"><i class="bi bi-arrow-up"></i></button>');
-const scrollTopBtn = document.getElementById("scroll-to-top");
-scrollTopBtn.style.display = "none";
-scrollTopBtn.addEventListener("click", function () {
-  window.scrollTo(0, 0);
-});
-window.addEventListener("scroll", function () {
-  if (window.scrollY > 128) {
-    scrollTopBtn.style.display = "block";
-  } else {
-    scrollTopBtn.style.display = "none";
-  }
-});
