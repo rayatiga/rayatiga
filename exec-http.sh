4f371ab64df55d4e27e480489177e8a5c2756162
bydzen <svnteentthones@gmail.com>
2025-02-05 14:27:36 +0700

    delete execute shells

--- exec-http.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-http-server public -p 5500 -c-1 --ext html
--- exec-wrangler.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-printf "Deploying public to [rayatiga] Cloudflare pages project...\n\n"
-wrangler pages deploy public --project-name rayatiga
